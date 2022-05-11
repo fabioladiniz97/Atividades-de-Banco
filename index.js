37470f18cde48270c6903c8ab964dcc551925259
Fabíola Diniz <fabioladiniz36@hotmail.com>
2022-05-11 15:00:41 -0300

    Aula dia 11-05-22
    atividade de banco II

--- index.js
+++ index.js
@@ -1,38 +1,8 @@
-let map;
-let info;
+//console.log('Hello World'); primeiro teste 
 
-let centro ={ lat: -6.895124480428826, lng: -38.56747736517121 };
-let marker;
+//require ('dotenv').config(); // 'esconder' informações do banco 
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: centro,
-    zoom: 14,
-    zoomControl: false,
-    MapTypeId: google.maps.MapTypeId.ROADMAP
-  });
+//console.log(process.env.NOME);
 
-  marker=new google.maps.Marker({
-    position: centro,
-    map,
-    title: "oiiiii!",
-    draggable: true,
-    animation: google.maps.Animation.BOUNCE
-  });
-
-  marker.addListener("dblclick", ()=>{
-    info = new google.maps.InfoWindow({
-      content: ""+marker.getPosition(),
-    }).open({
-    anchor: marker,
-    map,
-    shouldFocus: false,
-    });
-  });
-
-  map.addListener("click", (evt)=>{
-     marker.setPosition(evt.latLng);
-  });
-}
-
-window.initMap = initMap;
+const sequelize = require('./banco/bd');
+console.log(sequelize);
